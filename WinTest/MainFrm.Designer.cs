04b577fa2b71a0a1712f83a13e76af75f83d2b87
超级_小小 <wojiaotanghao@qq.com>
2018-02-06 09:16:12 +0800

    updaet
    updaet

---
+++ WinTest/MainFrm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WinTest
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtMonitorKey = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnNav = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(13, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(632, 511);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtMonitorKey
+            // 
+            this.txtMonitorKey.Location = new System.Drawing.Point(667, 35);
+            this.txtMonitorKey.Multiline = true;
+            this.txtMonitorKey.Name = "txtMonitorKey";
+            this.txtMonitorKey.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMonitorKey.Size = new System.Drawing.Size(455, 92);
+            this.txtMonitorKey.TabIndex = 1;
+            this.txtMonitorKey.Text = "www.baidu.com";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(667, 156);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(455, 390);
+            this.txtResult.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(667, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "监视URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(667, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "监视请求";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "URL：";
+            // 
+            // btnNav
+            // 
+            this.btnNav.Location = new System.Drawing.Point(577, 6);
+            this.btnNav.Name = "btnNav";
+            this.btnNav.Size = new System.Drawing.Size(68, 23);
+            this.btnNav.TabIndex = 3;
+            this.btnNav.Text = "跳转";
+            this.btnNav.UseVisualStyleBackColor = true;
+            this.btnNav.Click += new System.EventHandler(this.btnNav_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(53, 7);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(496, 21);
+            this.txtUrl.TabIndex = 5;
+            this.txtUrl.Text = "https://peter.sh/experiments/chromium-command-line-switches/";
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 558);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNav);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtMonitorKey);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainFrm";
+            this.Text = "测试窗口";
+            this.Load += new System.EventHandler(this.MainFrm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtMonitorKey;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnNav;
+        private System.Windows.Forms.TextBox txtUrl;
+    }
+}
+
---
+++ WinTest/MainFrm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Tangh_Chrome;
+
+namespace WinTest
+{
+    public partial class MainFrm : Form
+    {
+        private ChromeHelper chrome = null;
+
+        public MainFrm()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void btnSetKey_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            #region chrome1
+            this.chrome = new ChromeHelper("https://peter.sh/experiments/chromium-command-line-switches/");
+            this.chrome.NotifyResult += chrome_NotifyResult;
+            chrome.InitHelper();
+            List<string> list = new List<string>();
+            list.AddRange(this.txtMonitorKey.Text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries).ToList());
+
+            chrome.SetHtmlUrl(list);
+            var browser1 = chrome.Init();
+            AddBrowserToPanel(browser1, panel1);
+
+            #endregion
+        }
+
+        private void chrome_NotifyResult(string obj)
+        {
+            this.ShowMsg(obj);
+        }
+
+        private void AddBrowserToPanel(Control browser, Panel panel)
+        {
+            this.Invoke(new Action<Panel>(p =>
+            {
+                if (p.Controls.Count > 0)
+                {
+                    p.Controls.RemoveAt(0);
+                }
+
+                p.Controls.Add(browser);
+                p.Update();
+            }), panel);
+        }
+
+        private void btnNav_Click(object sender, EventArgs e)
+        {
+            this.chrome.JumpUrl(this.txtUrl.Text);
+        }
+
+        private void ShowMsg(string msg)
+        {
+            this.Invoke(new Action<TextBox>(p =>
+            {
+                if (p.TextLength > 30000)
+                {
+                    p.Clear();
+                }
+
+                p.AppendText(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "\r\n" + msg + "\r\n\r\n");
+            }), this.txtResult);
+        }
+    }
+}
